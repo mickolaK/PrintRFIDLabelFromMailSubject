f67aa808c694629ee4cda476bfdf5da65ce0b5f5
Nikolai <konevetsnikolay@gmail.com>
2020-04-06 15:22:09 +0700

    vice versa and back

--- PrintRFIDLabelFromMailSubject/Program.cs
+++ PrintRFIDLabelFromMailSubject/Program.cs
@@ -9,15 +9,16 @@ namespace PrintRFIDLabelFromMailSubject
         static void Main(string[] args)
         {
             var configuration = new ConfigurationBuilder()
-                .AddJsonFile("appconfig.json")
+                .AddJsonFile("appconfig.json",optional:false,reloadOnChange:true)
                 .Build();
-            
+            var label = new RFIDLabel(configuration["PrinterIP"], configuration["PrinterPort"])
+                .SendRFIDLabelToPrinter(RFIDString);
+            var printerport = configuration.
             var mailport =configuration["MailPort"];
             var RFIDString = new SubjectFromMail(configuration["MailHost"], configuration["MailUsername"], configuration["MailPassword"], configuration["MailPort"], configuration["MailEnableSSL"])
                 .GetSubjectFromMail();
             
-            var label = new RFIDLabel(configuration["PrinterIP"], configuration["PrinterPort"])
-                .SendRFIDLabelToPrinter(RFIDString);
+            
         }
     }
 }
