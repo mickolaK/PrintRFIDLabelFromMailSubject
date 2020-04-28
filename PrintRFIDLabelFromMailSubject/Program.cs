d11d27ca599c50249edd67c53e20bb4b93339401
Nikolai <konevetsnikolay@gmail.com>
2020-04-29 00:36:01 +0700

    Autofac working well

--- PrintRFIDLabelFromMailSubject/Program.cs
+++ PrintRFIDLabelFromMailSubject/Program.cs
@@ -11,15 +11,20 @@ namespace PrintRFIDLabelFromMailSubject
         {
             
             var builder = new ContainerBuilder();
+            builder.RegisterType<GetConfigFromJson>().As<IConfig>();
             builder.RegisterType<SubjectFromMail>();
             builder.RegisterType<RFIDLabel>();
+
+
             var container = builder.Build();
             var config = container.Resolve<IConfig>();
             var rfidString = container.Resolve<SubjectFromMail>().GetSubjectFromMail();
             var label = container.Resolve<RFIDLabel>().SendRFIDLabelToPrinter(rfidString);
 
+            
+            
+            
             //var config = new GetConfigFromJson();
-
             /*var rfidString = new SubjectFromMail(config)
                 .GetSubjectFromMail();
             var label = new RFIDLabel(config)
